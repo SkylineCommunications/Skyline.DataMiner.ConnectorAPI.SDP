9d74ca684c41ac9e73425a34364968915c931ed7
Tom Waterbley <tom.waterbley@skyline.be>
2023-10-19 15:18:47 +0200

    Add newline

--- ConnectorAPI.SDP/SetSDPConfigurationMessage.cs
+++ ConnectorAPI.SDP/SetSDPConfigurationMessage.cs
@@ -5,6 +5,7 @@ using Skyline.DataMiner.Core.InterAppCalls.Common.CallSingle;
 public class SetSDPConfigurationMessage : Message
 {
     public Guid Id { get; set; }
+
     public string SDPFileContent { get; set; }
 
     public DateTime Timestamp { get; set; }
