2162835d070ab77cb80715746c544e4ec9aafcc5
Rade Radovanović <rade.radovanovic@skyline.be>
2023-10-20 10:11:47 +0200

    Fixing indentation.

--- ConnectorAPI.SDP/SetSDPConfigurationMessage.cs
+++ ConnectorAPI.SDP/SetSDPConfigurationMessage.cs
@@ -4,11 +4,12 @@
 
     using Skyline.DataMiner.Core.InterAppCalls.Common.CallSingle;
 
-public class SetSDPConfigurationMessage : Message
-{
-    public Guid Id { get; set; }
+    public class SetSDPConfigurationMessage : Message
+    {
+        public Guid Id { get; set; }
 
-    public string SDPFileContent { get; set; }
+        public string SDPFileContent { get; set; }
 
-    public DateTime Timestamp { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
 }
