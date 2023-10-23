ef2585e7bfee8929a912242eb203e32b57bcf5b0
Tom Waterbley <tom.waterbley@skyline.be>
2023-10-23 16:21:25 +0200

    add KnownTypes

---
+++ ConnectorAPI.SDP/KnownTypes.cs
@@ -0,0 +1,19 @@
+﻿namespace Skyline.DataMiner.ConnectorAPI.SDP
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Reflection;
+
+	using Skyline.DataMiner.Core.InterAppCalls.Common.CallSingle;
+
+	public static class KnownTypes
+	{
+		public static IEnumerable<Type> GetList()
+		{
+			return Assembly.GetExecutingAssembly()
+				.GetTypes()
+				.Where(x => x.IsSubclassOf(typeof(Message)));
+		}
+	}
+}
